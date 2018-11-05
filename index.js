1156ba0db8c14e49bec245d4f3329879941aec73
Taylor Stapleton <stapletontaylor@hotmail.com>
2018-11-04 22:02:27 -0700

    Dynamic module loading is done.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const Discord = require('discord.js');
-
 _langfiles.set('musicbot', require('./lib/lang.json'));
 _loader(require('path').join(__dirname, 'commands'));
 _connections.get('database').promise.then(() => {
