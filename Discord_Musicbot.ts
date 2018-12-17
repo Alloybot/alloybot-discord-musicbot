0a599bb555e83d504026aa8fa545938664276f2f
Taylor Stapleton <stapletontaylor@hotmail.com>
2018-12-16 22:33:14 -0700

    LETS GET IT

--- Discord_Musicbot.ts
+++ Discord_Musicbot.ts
@@ -1,13 +1,18 @@
-import { default as Alloybot, Type, Util } from '../../Alloybot';
+import { default as Alloybot, Type, Util, ConfigBuilder } from '../../Alloybot';
 import { EventEmitter } from 'events';
 
-export class Discord_Musicbot extends EventEmitter implements Type.IPlugin {
+export default class Discord_Musicbot extends EventEmitter implements Type.IPlugin {
   public readonly name: string = 'Discord-Musicbot';
   public readonly dependencies: string[] = ['Commander', 'Discord', 'MongoDB'];
   public readonly dependants: Type.IPlugin[] = Alloybot.getDependants(this.name);
+  public config;
 
   constructor() {
     super();
+    let Config: ConfigBuilder = new ConfigBuilder('Discord-Musicbot', require('./package.json').version);
+    Config.addOption('Name', ['string'], 'Name of the Musicbot');
+    Config.close();
+    this.config = Config.getConfig();
   }
 }
 
